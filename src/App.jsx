b5f670a78785704cc402fe8671f8045348fdb78c
Erik Varga <viglew@gmail.com>
2025-02-02 16:38:00 -0500

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -33,36 +33,48 @@ const App = () => {
     }
   })
 
-
-
   return (
     <ThemeProvider theme={theme}>
-      <main className='h-screen overflow-x-hidden antialiased p-5 bg-white dark:bg-slate-950 text-neutral-800 dark:text-neutral-300 transition-all duration-500'>
-        <div className="flex justify-between">
+      <main className='h-screen flex flex-col justify-between overflow-x-hidden antialiased p-5 bg-white dark:bg-slate-950 text-neutral-800 dark:text-neutral-300 transition-all duration-500'>
+        <div>
           <a href="/">
-            <div className='flex items-center gap-2 font-bold'>
+            <div className='flex items-center justify-center gap-2 font-thin text-3xl'>
                 <RiSurveyLine size={25} />
                 Multi-Step App
             </div>
           </a>
         </div>
-        <div className="mt-10 flex justify-center">
-          <Stepper style={{ width: '45%' }} activeStep={currentStep - 1} orientation='horizontal'>
-            <Step>
-              <StepLabel></StepLabel>
-            </Step>
-            <Step>
-              <StepLabel></StepLabel>
-            </Step>
-            <Step>
-              <StepLabel></StepLabel>
-            </Step>
-          </Stepper>
+        <div>
+          <div className="hidden sm:flex mt-10 justify-center">
+            <Stepper style={{ width: '45%' }} activeStep={currentStep - 1} orientation='horizontal'>
+              <Step>
+                <StepLabel></StepLabel>
+              </Step>
+              <Step>
+                <StepLabel></StepLabel>
+              </Step>
+              <Step>
+                <StepLabel></StepLabel>
+              </Step>
+            </Stepper>
+          </div>
+          <div className="sm:hidden mt-10 flex justify-center">
+            <Stepper style={{ width: '100%' }} activeStep={currentStep - 1} orientation='horizontal'>
+              <Step>
+                <StepLabel></StepLabel>
+              </Step>
+              <Step>
+                <StepLabel></StepLabel>
+              </Step>
+              <Step>
+                <StepLabel></StepLabel>
+              </Step>
+            </Stepper>
+          </div>
+          <div className="mt-4 flex justify-center"> 
+            { showStep(currentStep) }
+          </div>
         </div>
-        <div className="mt-4 flex justify-center"> 
-          { showStep(currentStep) }
-        </div>
-
         {finalData.length > 0 && (
           <DisplayData />
         )}
--- src/components/DisplayData.jsx
+++ src/components/DisplayData.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react'
 import { multiStepContext } from './StepContext'
-import { Table, TableContainer, StepLabel, TableHead, TableRow, TableCell, TableBody, Typography } from '@mui/material';
+import { Table, TableContainer, StepLabel, TableHead, TableRow, TableCell, TableBody, Typography, Card, CardContent } from '@mui/material';
 
 const DisplayData = () => {
     const { finalData } = useContext(multiStepContext);
 
   return (
     <Typography>
-        <div className='mt-5 text-sm'>
+        <div className='hidden sm:flex mt-5 text-sm'>
             <TableContainer style={{ display: 'flex', justifyContent: 'center' }}>
                 <Table border='1' style={{ width: '70%', justifyContent: 'center' }} size='small' aria-label='caption table'>
                     <TableHead>
@@ -40,6 +40,14 @@ const DisplayData = () => {
                     </TableBody>
                 </Table>
             </TableContainer>
+        </div>
+        <div className='flex sm:hidden my-5'>
+            <ul>
+                <span className='underline'>NAME</span>
+                {finalData.map((data, index) => (
+                    <li key={index}>{data.firstName} {data.lastName} {data.contactNumber}</li>
+                ))}
+            </ul>
         </div>
     </Typography>
   )
